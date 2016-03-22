8e23acd9a12f833861fbbff436d15706854f6c5b
sndekoning <sylvester.de.koning90@gmail.com>
2016-03-22 12:47:16 +0100

    Finalizing script

--- courseproject.R
+++ courseproject.R
@@ -14,10 +14,42 @@ if(!dir.exists("./data")){
     }
 }
 
+library(caret)
+
 # Reading in the data.
 training <- read.csv("./data/training.csv", na.strings = c("", "NA"))
-testing <- read.csv("./data/testing.csv")
+testing <- read.csv("./data/testing.csv", na.strings = "NA")
 
-#Removing variables with large amounts, >0.9, of missing values.
+# Removing unneccesary variables such as participant name, timestamps, etc.
 
-names <- colnames(training)
+training <- training[, -c(1:7)]
+# testing <- testing[, -c(1:7)]
+
+#Removing variables with large (>0.9) amounts of missing values.
+for(i in 1:ncol(training)){
+    na[i] <- sum(is.na(training[, i]))
+}
+high.na <- names(training[, na > (0.9*nrow(training))])
+training <- training[, !names(training) %in% high.na]
+# testing <- testing[, !names(testing) %in% high.na]
+
+# Removing variables which are highly (>0.9) correlated to each other.
+corr <- cor(training[, -length(training)])
+cutoff <- findCorrelation(corr)
+training <- training[, -cutoff]
+
+# Creating a new partition to test the model on.
+in.train <- createDataPartition(y = training$classe, p = .7, list = FALSE)
+
+mod.train <- training[in.train ,]
+mod.test <- training[ -in.train ,]
+
+# fitting a random forest model.
+rf.model <- train(classe ~ ., method = "rf", data = mod.train)
+
+# predicting on the cross validation set.
+pred.cv <- predict(rf.model, mod.test)
+
+confusionMatrix(pred.cv, mod.test$classe)
+
+final.pred <- predict(rf.model, testing)
